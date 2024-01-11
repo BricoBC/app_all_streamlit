51bd77b4df5f8531ac4fbc9aba2b5bf67c65b019
BricoBC <barajas.c.bruno.n@gmail.com>
2024-01-11 13:07:52 -0600

    Add title a every chart

--- pages/display_charts.py
+++ pages/display_charts.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
 import numpy as np
+import graphviz
+import plotly.figure_factory as ff
+
 
 df = pd.DataFrame((
     {
@@ -12,18 +17,79 @@ df = pd.DataFrame((
         "F": "foo",
     }
 ))
+df_map = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist(),
+})
 
+st.subheader('AREA_CHART')
 st.area_chart(df)
-st.bar_chart(df)
-st.line_chart(df)
-# st.map(data=[(19.432688, -99.133209), (20.677162, -97.127186), (19.024181, -98.210968)], zoom=4)
 
+st.subheader('BAR_CHART')
+st.bar_chart(df)
+st.subheader('LINE_CHART')
+st.line_chart(df)
+st.subheader('MAP')
+st.map(df_map, latitude='col1', longitude='col2', size='col3', color='col4')
+
+st.subheader('SCATTER_CHART')
 st.scatter_chart(df)
 
-# st.altair_chart(chart)
-# st.bokeh_chart(fig)
-# st.graphviz_chart(fig)
-# st.plotly_chart(fig)
+st.subheader('PYPLOT')
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+
+st.subheader('ALTAIR_CHART')
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+c = (
+   alt.Chart(chart_data)
+   .mark_circle()
+   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+st.altair_chart(c, use_container_width=True)
+
+
+st.subheader('GRAPHVIZ_CHART')
+graph = graphviz.Digraph()
+graph.edge('run', 'intr')
+graph.edge('intr', 'runbl')
+graph.edge('runbl', 'run')
+graph.edge('run', 'kernel')
+graph.edge('kernel', 'zombie')
+graph.edge('kernel', 'sleep')
+graph.edge('kernel', 'runmem')
+graph.edge('sleep', 'swap')
+graph.edge('swap', 'runswap')
+graph.edge('runswap', 'new')
+graph.edge('runswap', 'runmem')
+graph.edge('new', 'runmem')
+graph.edge('sleep', 'runmem')
+
+st.graphviz_chart(graph)
+
+
 # st.pydeck_chart(chart)
-# st.pyplot(fig)
-st.vega_lite_chart(df)
+
+
+st.subheader('VEGA_LITE_CHART')
+chart_data = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+
+st.vega_lite_chart(
+   chart_data,
+   {
+       "mark": {"type": "circle", "tooltip": True},
+       "encoding": {
+           "x": {"field": "a", "type": "quantitative"},
+           "y": {"field": "b", "type": "quantitative"},
+           "size": {"field": "c", "type": "quantitative"},
+           "color": {"field": "c", "type": "quantitative"},
+       },
+   },
+)
