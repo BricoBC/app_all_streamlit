6c2da048e50c795f5e84f52066eff1f8bf1138fe
BricoBC <barajas.c.bruno.n@gmail.com>
2024-01-11 11:43:00 -0600

    Add components to sidebar

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title('Proyecto para explicar todos los comandos.')
 st.markdown('- Display text') # see *
+
+
+st.sidebar.radio('Select one:', [3, 4])
+# Or use "with" notation:
+# with st.sidebar:
+#   st.radio('Select one:', [1, 2])
